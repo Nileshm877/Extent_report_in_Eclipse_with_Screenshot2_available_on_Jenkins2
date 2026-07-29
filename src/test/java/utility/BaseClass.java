0d2fcede3b3a0b55d7269ce2b56e1cae94c8bc00
Nileshm87 <nilesh.marathe89@gmail.com>
2026-07-29 13:25:37 +0530

    changes

--- src/test/java/utility/BaseClass.java
+++ src/test/java/utility/BaseClass.java
@@ -1,12 +1,16 @@
 package utility;
 
+import java.io.File;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.cucumber.java.Scenario;
-
+import java.io.File;
+import java.nio.file.Files;
+import org.apache.commons.io.FileUtils;
 public class BaseClass 
 {
 	public static WebDriver driver;
@@ -21,9 +25,26 @@ public class BaseClass
 	public void captureScreenshot(Scenario scenario) 
 	{  try 
 	    {
-	        byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+		
 
-	        scenario.attach(screenshot, "image/png", "Step Screenshot");
+		   
+
+		        File src = ((TakesScreenshot) driver)
+		                .getScreenshotAs(OutputType.FILE);
+
+		        String fileName = System.currentTimeMillis() + ".png";
+
+		        File dest = new File("target/screenshots/" + fileName);
+
+		        FileUtils.copyFile(src, dest);
+
+		        byte[] screenshot = Files.readAllBytes(dest.toPath());
+
+		        scenario.attach(screenshot, "image/png", "Step Screenshot");
+
+		    
+
+		
          } 
 	    catch (Exception e)
 	    {
